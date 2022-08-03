25c94dfc4ef7e245dadd46864576b52218ec93d8
Yang Liu <yangliu2000gm@gmail.com>
2022-08-03 17:52:38 +0800

    20220803-a

--- describeTimeSeries/HowToFindPeakPointOnTimeSeries.py
+++ describeTimeSeries/HowToFindPeakPointOnTimeSeries.py
@@ -68,10 +68,71 @@ class real_time_peak_detection():
         return self.signals[i]
 
 
+def thresholding_algo(y, lag, threshold, influence):
+    signals = np.zeros(len(y))
+    filteredY = np.array(y)
+    avgFilter = [0]*len(y)
+    stdFilter = [0]*len(y)
+    avgFilter[lag - 1] = np.mean(y[0:lag])
+    stdFilter[lag - 1] = np.std(y[0:lag])
+    for i in range(lag, len(y)):
+        if abs(y[i] - avgFilter[i-1]) > threshold * stdFilter [i-1]:
+            if y[i] > avgFilter[i-1]:
+                signals[i] = 1
+            else:
+                signals[i] = -1
 
-lag = 3
-threshold = 25
+            filteredY[i] = influence * y[i] + (1 - influence) * filteredY[i-1]
+            avgFilter[i] = np.mean(filteredY[(i-lag+1):i+1])
+            stdFilter[i] = np.std(filteredY[(i-lag+1):i+1])
+        else:
+            signals[i] = 0
+            filteredY[i] = y[i]
+            avgFilter[i] = np.mean(filteredY[(i-lag+1):i+1])
+            stdFilter[i] = np.std(filteredY[(i-lag+1):i+1])
+
+    return dict(signals = np.asarray(signals),
+                avgFilter = np.asarray(avgFilter),
+                stdFilter = np.asarray(stdFilter))
+
+
+
+y = np.array([1,1,1.1,1,0.9,1,1,1.1,1,0.9,1,1.1,1,1,0.9,1,1,1.1,1,1,1,1,1.1,0.9,1,1.1,1,1,0.9,
+       1,1.1,1,1,1.1,1,0.8,0.9,1,1.2,0.9,1,1,1.1,1.2,1,1.5,1,3,2,5,3,2,1,1,1,0.9,1,1,3,
+       2.6,4,3,3.2,2,1,1,0.8,4,4,2,2.5,1,1,1])
+
+# Settings: lag = 30, threshold = 5, influence = 0
+lag = 30
+threshold = 5
 influence = 0
+
+# Run algo with settings from above
+result = thresholding_algo(y, lag=lag, threshold=threshold, influence=influence)
+
+# Plot result
+plt.subplot(211)
+plt.plot(np.arange(1, len(y)+1), y)
+
+plt.plot(np.arange(1, len(y)+1),
+           result["avgFilter"], color="cyan", lw=2)
+
+plt.plot(np.arange(1, len(y)+1),
+           result["avgFilter"] + threshold * result["stdFilter"], color="green", lw=2)
+
+plt.plot(np.arange(1, len(y)+1),
+           result["avgFilter"] - threshold * result["stdFilter"], color="green", lw=2)
+
+plt.subplot(212)
+plt.step(np.arange(1, len(y)+1), result["signals"], color="red", lw=2)
+plt.ylim(-1.5, 1.5)
+plt.show()
+
+
+
+
+# lag = 3
+# threshold = 25
+# influence = 0
 
 
 # arrini = [
@@ -81,29 +142,29 @@ influence = 0
 #         4,30,30.2,20,1,10,00.8,40,4,20,
 #     ]
 
-arrini = [
-        10.1,10,1,10.1
-    ]    
+# arrini = [
+#         10.1,10,1,10.1
+#     ]    
 
-y = [
-        1,10,60.1,10,00.9,10,1,10.1,10,00.9,
-        1,10.1,10,1,00.9,10,1,10.1,10,1,
-        1,10,10.1,00.9,10,10.1,10,1,00.9,10,
-        10.1,10,1,10.1,10,00.8,00.9,10,10.2,00.9,
-        1,10,10.1,10.2,10,10.5,10,3,20,5,
-        3,20,1,10,1,00.9,10,1,30, 20.6,
-        4,30,30.2,20,1,10,00.8,40,4,20,
-        20.5,10,1,1
-    ]
+# y = [
+#         1,10,60.1,10,00.9,10,1,10.1,10,00.9,
+#         1,10.1,10,1,00.9,10,1,10.1,10,1,
+#         1,10,10.1,00.9,10,10.1,10,1,00.9,10,
+#         10.1,10,1,10.1,10,00.8,00.9,10,10.2,00.9,
+#         1,10,10.1,10.2,10,10.5,10,3,20,5,
+#         3,20,1,10,1,00.9,10,1,30, 20.6,
+#         4,30,30.2,20,1,10,00.8,40,4,20,
+#         20.5,10,1,1
+#     ]
 
-rtpd = real_time_peak_detection(arrini, lag, threshold, influence)
+# rtpd = real_time_peak_detection(arrini, lag, threshold, influence)
 
-num = 0
-while num < len(y):
-    res = rtpd.thresholding_algo(y[num])
-    print(y[num],res)
-    time.sleep(5)
-    num += 1
+# num = 0
+# while num < len(y):
+#     res = rtpd.thresholding_algo(y[num])
+#     print(y[num],res)
+#     time.sleep(5)
+#     num += 1
 
 
 
