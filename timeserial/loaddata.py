6f85efbfe7608bbe5be81f9a0b10c9482c5e2816
Yang Liu <yangliu2000gm@gmail.com>
2023-05-09 23:04:14 +0800

    20230510

---
+++ timeserial/loaddata.py
@@ -0,0 +1,9 @@
+from pandas import read_csv
+series = read_csv('../../pythonsample/Datasets-master/daily-total-female-births.csv', header=0, index_col=0, parse_dates=True,
+squeeze=True)
+
+# series = read_csv('../../pythonsample/Datasets-master/daily-total-female-births.csv', header=0, index_col=0, parse_dates=True)
+
+# series = read_csv('../../pythonsample/Datasets-master/daily-total-female-births.csv')
+print(type(series))
+print(series.head())
