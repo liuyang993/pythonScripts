c76dd7bb86228ad9eaa3a0efcbbcc9295dccfa9c
Yang Liu <yangliu2000gm@gmail.com>
2023-05-16 23:43:09 +0800

    20230517

--- padasTest/shubhamsharmaAnswers.py
+++ padasTest/shubhamsharmaAnswers.py
@@ -8,33 +8,52 @@ import numpy as np
 import time
 
 
-jsonstring = [[22415, 7, '2022-02-15 00:00:00', 'KEY', np.NAN],
-              [22415, 7, '2022-02-24 00:00:00', 'MELOXICA', np.NAN], 
-              [22415, 7, '2022-10-11 00:00:00', 'CEPFR', 12.0], 
-              [22415, 7, '2022-10-11 00:00:00', 'MELOXICA', np.NAN], 
-              [25302, 8, '2022-06-05 00:00:00', 'TOX FL', 11.0],
-                [25302, 8, '2022-06-05 00:00:00', 'FLUNIX', np.NAN], 
-                [25302, 8, '2022-06-07 00:00:00', 'FLUNIX', np.NAN], 
-                [25302, 8, '2022-07-07 00:00:00', 'MAS', np.NAN], 
-                [25302, 8, '2022-07-07 00:00:00', 'FLUNIX', np.NAN], 
-                [26662, 8, '2022-07-08 00:00:00', 'FR', 12.0], 
-                [26662, 8, '2022-07-08 00:00:00', 'FLUNIX', np.NAN], 
-                [26662, 8, '2022-07-17 00:00:00', 'SFFR', 12.0], 
-                [26662, 8, '2022-07-17 00:00:00', 'MELOXICA', np.NAN]]
+# jsonstring = [[22415, 7, '2022-02-15 00:00:00', 'KEY', np.NAN],
+#               [22415, 7, '2022-02-24 00:00:00', 'MELOXICA', np.NAN], 
+#               [22415, 7, '2022-10-11 00:00:00', 'CEPFR', 12.0], 
+#               [22415, 7, '2022-10-11 00:00:00', 'MELOXICA', np.NAN], 
+#               [25302, 8, '2022-06-05 00:00:00', 'TOX FL', 11.0],
+#                 [25302, 8, '2022-06-05 00:00:00', 'FLUNIX', np.NAN], 
+#                 [25302, 8, '2022-06-07 00:00:00', 'FLUNIX', np.NAN], 
+#                 [25302, 8, '2022-07-07 00:00:00', 'MAS', np.NAN], 
+#                 [25302, 8, '2022-07-07 00:00:00', 'FLUNIX', np.NAN], 
+#                 [26662, 8, '2022-07-08 00:00:00', 'FR', 12.0], 
+#                 [26662, 8, '2022-07-08 00:00:00', 'FLUNIX', np.NAN], 
+#                 [26662, 8, '2022-07-17 00:00:00', 'SFFR', 12.0], 
+#                 [26662, 8, '2022-07-17 00:00:00', 'MELOXICA', np.NAN]]
 
-df = pd.DataFrame(jsonstring)
+# df = pd.DataFrame(jsonstring)
 
-df.columns = ['ID', 'LACT','Date','Remark','QUARTER']
-
-
-#解决方法
-df['Date'] = pd.to_datetime(df['Date'])
-df = df.sort_values(['ID','LACT','Date'])
+# df.columns = ['ID', 'LACT','Date','Remark','QUARTER']
 
 
+# #解决方法
+# df['Date'] = pd.to_datetime(df['Date'])
+# df = df.sort_values(['ID','LACT','Date'])
 
-groups = df.groupby(['ID','LACT'])['Date'].diff().dt.days.fillna(0).gt(6).cumsum()
-f = lambda x: x.ffill().bfill()
-df['QUARTER'] = df.groupby(['ID','LACT', groups])['QUARTER'].transform(f)
-print (df)
 
+
+# groups = df.groupby(['ID','LACT'])['Date'].diff().dt.days.fillna(0).gt(6).cumsum()
+# f = lambda x: x.ffill().bfill()
+# df['QUARTER'] = df.groupby(['ID','LACT', groups])['QUARTER'].transform(f)
+# print (df)
+
+
+#第二个  https://stackoverflow.com/questions/67841754/get-values-of-previous-rows-as-list/67842195#67842195
+
+def some_function_v1(df):
+  df['foo1'] = df.foo.shift(1)
+  df['foo2'] = df.foo.shift(2)
+  df['foo3'] = df.foo.shift(3)
+
+
+  df['bar'] = df.apply(lambda x: [x['foo1'],x['foo2'],x['foo3']], axis=1)
+  # print(df)
+  df = df.drop(columns=['foo1','foo2','foo3'])
+  return df
+
+data = { 'foo':['a','b','c','d','e','f','g']}
+df = pd.DataFrame(data)
+
+df = some_function_v1(df)
+print(df)
